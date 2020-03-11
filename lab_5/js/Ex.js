27386a4193ca5bef55ec1527f8dc4d9c89735e92
PierreBls <pierre.blais@epita.fr>
2020-03-11 16:12:09 +0000

    11/03

---
+++ lab_5/js/Ex.js
@@ -0,0 +1,32 @@
+// Question 3
+
+var firstName = "Pierre";
+
+// Question 4 & 5
+
+var favouriteColor = "Blue";
+var favouriteFood = "Pasta";
+
+// Question 8
+
+console.log(firstName, favouriteColor, favouriteFood);
+
+// Question 9
+
+// Console log print my firstname, my favouriteColor and my favouriteFood
+
+// Question 10
+
+var one = "red";
+var two = "orange";
+var three = "yellow";
+var four = "green";
+var five = "blue";
+var six = "indogo";
+var seven = "violet";
+var phrase = "A rainbow is formed by the sunlight. Shining through rain drops sosmall. The rain bends the light from the sunshine, and colors the sky for us all. "
+var endphrase = "is what we see. Rainbow colours, are made with light bendingyou see!"
+
+// Question 11
+
+console.log(phrase + " " + one + ", " + two, + ", " + three + ", " + four + ", " +  five + ", " + six + ", " + seven + endphrase);
