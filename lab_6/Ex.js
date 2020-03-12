922165c56ff9e39a213b2cec7e116692fe005c1f
PierreBls <pierre.blais@epita.fr>
2020-03-12 16:05:23 +0000

    12/03

---
+++ lab_6/Ex.js
@@ -0,0 +1,39 @@
+// Question 4
+var array = ["a","z","e","r","t","y","u"];
+
+function myAlphabetLength(array)
+{
+    console.log(array.length);
+}
+//Question 5
+
+var Planets = ["aze","azer","azert", "azerty", "azertyu"];
+
+//Question 6 
+
+console.log(Planets[0]);
+console.log(Planets[1]);
+console.log(Planets[2]);
+console.log(Planets[3]);
+console.log(Planets[4]);
+
+// Question 7
+
+var wowDatatypes = [1, "one", 1.0];
+
+console.log(wowDatatypes[0]);
+console.log(wowDatatypes[1]);
+console.log(wowDatatypes[2]);
+
+// Question 11
+
+var Hello = function(){
+    return "Hello World!";
+}
+
+// Question 12
+
+console.log(Hello);
+
+var x= Hello();
+console.log(x);
